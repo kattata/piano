6d5d33a8a60adbf138d3097bf62b12b08ed396e7
Kasia <kasia.laniecka@gmail.com>
2020-05-02 18:56:30 +0200

    almost finished

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
+    audio.volume = 0.1;
     audio.play();
     audio.currentTime = 0;
-    console.log(e.keyCode);
+    key.classList.add('playing');
 }
 
 window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTrasition));
